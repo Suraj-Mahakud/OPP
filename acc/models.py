2efdb15af8a310f016a10291e75cbff8fe5b40ad
Suraj-Mahakud <suraj.mahakud@shivila.com>
2022-12-06 11:59:47 +0530

    OP

---
+++ acc/models.py
@@ -0,0 +1,26 @@
+from pyexpat import model
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    full_name = models.CharField(max_length=100, default=False)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=12)
+    password2 = models.CharField(max_length=12)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
